6167fa0aaeabda4c1fb0a2b1617fd50f57b8425b
svnrnns <haruueel@gmail.com>
2024-08-07 13:38:19 +0200

    feat: created hooks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import Aurora from "./Aurora.js";
+import Aurora from './Aurora.js';
+
+// Aurora Instance
+const defaultAurora = new Aurora();
+
+// Hooks
+const useCall = (method, endpoint, headers, params, config, abortController) =>
+  defaultAurora.call(
+    method,
+    endpoint,
+    headers,
+    params,
+    config,
+    abortController
+  );
+const useGet = (endpoint, headers, params, config, abortController) =>
+  defaultAurora.get(endpoint, headers, params, config, abortController);
+const usePost = (endpoint, headers, params, config, abortController) =>
+  defaultAurora.post(endpoint, headers, params, config, abortController);
+const usePut = (endpoint, headers, params, config, abortController) =>
+  defaultAurora.put(endpoint, headers, params, config, abortController);
+const usePatch = (endpoint, headers, params, config, abortController) =>
+  defaultAurora.patch(endpoint, headers, params, config, abortController);
+const useDelete = (endpoint, headers, params, config, abortController) =>
+  defaultAurora.delete(endpoint, headers, params, config, abortController);
 
 export default Aurora;
+export { useCall, useGet, usePost, usePut, usePatch, useDelete };
